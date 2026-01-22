dbed0d08104407bec3d2b7b732f1aa242b7f40af
Henrik Wik <henrik.wik@outlook.com>
2026-01-22 20:48:34 +0100

    Update OMZ

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,7 @@
 sudo apt update && sudo apt upgrade -y
 
 sudo apt install -y zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sRUNZSH=no CHSH=no sh -c \
+  "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 curl -fsSL https://opencode.ai/install | zsh
