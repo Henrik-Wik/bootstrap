84b3e749bb742680f55dff8a4c7694b32ed9a426
Henrik Wik <126109206+Henrik-Wik@users.noreply.github.com>
2026-01-24 11:53:02 +0100

    Refactor bootstrap.sh for improved readability

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 
 sudo apt update && sudo apt upgrade -y
-sudo apt install python3 python3-pip python3-venv
-sudo apt install -y zsh curl git neovim gh wl-clipboard
+sudo apt install -y \
+python3 python3-pip python3-venv \
+zsh curl git neovim gh
 
 mkdir -p ~/repos
 
 curl -fsSL https://opencode.ai/install | bash
 
-RUNZSH=yes CHSH=yes sh -c \
+RUNZSH=no CHSH=no sh -c \
   "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 git clone https://github.com/Henrik-Wik/dotfiles ~/repos/dotfiles && ~/repos/dotfiles/install.sh
+
+chsh -s $(which zsh)
