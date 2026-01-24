37e6a47a7008c52d2e1c2cc576853d228b5ef7ff
Henrik Wik <henrik.wik@outlook.com>
2026-01-24 10:58:34 +0100

    update with dotfiles etc.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,14 @@
+#!/usr/bin/env bash
+
 sudo apt update && sudo apt upgrade -y
 sudo apt install python3 python3-pip python3-venv
-sudo apt install -y zsh curl git nvim gh
+sudo apt install -y zsh curl git neovim gh wl-clipboard
+
+mkdir -p ~/repos
 
 curl -fsSL https://opencode.ai/install | bash
 
 RUNZSH=yes CHSH=yes sh -c \
   "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+git clone https://github.com/Henrik-Wik/dotfiles ~/repos/dotfiles && ~/repos/dotfiles/install.sh
