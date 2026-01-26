92f0212c3e2dc61460cf2f2cc9338ce6f9604209
Henrik Wik <henrik.wik@outlook.com>
2026-01-26 20:35:12 +0100

    update with latest installs

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,15 +3,31 @@
 sudo apt update && sudo apt upgrade -y
 sudo apt install -y \
 python3 python3-pip python3-venv \
-zsh curl git neovim gh
+zsh curl git gh unzip ripgrep fzf tmux
 
+# Node.js (better than apt)
+curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+sudo apt install -y nodejs
+
+# tree-sitter-cli for python parsing
+npm install -g tree-sitter-cli
+
+# Neovim tarball install
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
+sudo rm -rf /opt/nvim-linux-x86_64
+sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
+rm nvim-linux-x86_64.tar.gz
+
+# create local repos folder
 mkdir -p ~/repos
 
-curl -fsSL https://opencode.ai/install | bash
-
+# OMZ
 RUNZSH=no CHSH=no sh -c \
   "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+# dotfiles
 git clone https://github.com/Henrik-Wik/dotfiles ~/repos/dotfiles && ~/repos/dotfiles/install.sh
 
+# change default shell to zsh
 chsh -s $(which zsh)
+
